7c79352d032b334cba801fb73635f5008bc32353
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-01 10:25:13 +0000

    Placeholder text replacement

--- components/simple_footer_component_1722507899.js
+++ components/simple_footer_component_1722507899.js
@@ -9,13 +9,13 @@ Vue.component("simple_footer_component_1722507899", {
                     <div class="flex">
                         <a id="footer-logo-link" href="#" class="flex-1 flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
                             <img id="footer-logo" src="./images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
-                            STARTER_TEMPLATE_APPLICATION_NAME
+                            TextCraft: The Writer's Toolbox
                         </a>
                     </div>
                 </div>
                 <div class="flex">
                     <div id="footer-text" class="flex-1 block text-sm text-center text-gray-500 dark:text-gray-400">
-                        STARTER_TEMPLATE_FOOTER_CONTENT
+                        Created by writers, for writers. TextCraft empowers you to build compelling narratives and engaging content with ease.
                     </div>
                 </div>
             </div>
